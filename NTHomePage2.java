6a87a3473b43621848ac8eb0a278002a1ba89263
“Pranav” <Pranav@gmail.com>
2022-08-18 19:30:31 +0530

    adding changes suggested by avish

--- NTHomePage2.java
+++ NTHomePage2.java
@@ -36,6 +36,7 @@ public class NTHomePage2 {
 	public NTHomePage2(WebDriver driver) {
 		this.driver=driver;
 		PageFactory.initElements(driver, this);
+		System.out.println("Pranav is updating this file 2");
 	System.out.println("Updating this file in qa1");
 	}
 
